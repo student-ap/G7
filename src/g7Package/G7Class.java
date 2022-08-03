ee47ade3acadf2951eee2f75cc01cacf2e8c84db
uspai <uspai@Win.fios-router.home>
2022-08-03 12:00:16 -0500

    Implemented...

--- src/g7Package/G7Class.java
+++ src/g7Package/G7Class.java
@@ -2,6 +2,7 @@ package g7Package;
 
 public class G7Class {
 	public static void main(String[] args) {
-		System.out.println("p r a c t i c i n g");
+		System.out.println("FirstName: John");
+		System.out.println("LastName: Cena");
 	}
 }
