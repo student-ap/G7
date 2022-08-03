a77e6ed268acf6edeae71f4126a6cc38d3be0eda
Ahmad Farid <110270702+student-ap@users.noreply.github.com>
2022-08-03 12:16:02 -0500

    Update G7Class.java

--- src/g7Package/G7Class.java
+++ src/g7Package/G7Class.java
@@ -4,7 +4,7 @@ public class G7Class {
 	public static void main(String[] args) {
 		System.out.println("FirstName: John");
 		System.out.println("LastName: Cena");
-		
+	}
 		
 		//addedd
 		public static void divide(int x, int y){
